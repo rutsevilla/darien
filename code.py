26f1acabfbd598ed19dd8a905d288fe9c6a0aee1
Rut <162347172+rutsevilla@users.noreply.github.com>
2025-10-09 14:11:46 +0200

    Update code.py

--- code.py
+++ code.py
@@ -379,12 +379,14 @@ for label in LABELS:
         "img": rgba_to_dataurl(rgba_i),
         "bounds": [w_i, s_i, e_i, n_i],  # [W,S,E,N]
     })
+<style>
 
 html = f"""
 <style>
   @font-face {{
     font-family: 'PoppinsLocal';
-    src: local('Poppins'), url('https://fonts.gstatic.com/s/poppins/v20/pxiEyp8kv8JHgFVrFJA.ttf') format('truetype');
+    src: url('Poppins-Regular.woff2') format('woff2'),
+         url('Poppins-Regular.ttf') format('truetype');
     font-weight: 400;
     font-style: normal;
   }}
