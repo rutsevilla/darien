b2b01ee23f4e958bd443fc7b1b2377ce8f69a431
Rut <162347172+rutsevilla@users.noreply.github.com>
2025-10-09 14:15:33 +0200

    Update code.py

--- code.py
+++ code.py
@@ -384,8 +384,7 @@ html = f"""
 <style>
   @font-face {{
     font-family: 'PoppinsLocal';
-    src: url('Poppins-Regular.woff2') format('woff2'),
-         url('Poppins-Regular.ttf') format('truetype');
+    src: local('Poppins'), url('https://fonts.gstatic.com/s/poppins/v20/pxiEyp8kv8JHgFVrFJA.ttf') format('truetype');
     font-weight: 400;
     font-style: normal;
   }}
